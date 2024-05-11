9fc48d52c99b2952c5e98710ddc716191a732e2d
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2024-05-11 11:53:13 +0000

    removing docker stuff

--- src/alpine-python/install.sh
+++ src/alpine-python/install.sh
@@ -2,14 +2,12 @@
 set -e
 
 # Install python/pip
-ENV PYTHONUNBUFFERED=1
-
 echo "Adding python 3"
-RUN apk add --update --no-cache python3 
+apk add --update --no-cache python3 
 
 echo "linking python3 to python"
 ln -sf python3 /usr/bin/python
 
 echo "Adding pip"
-RUN python3 -m ensurepip
-RUN pip3 install --no-cache --upgrade pip setuptools
+python3 -m ensurepip
+pip3 install --no-cache --upgrade pip setuptools
