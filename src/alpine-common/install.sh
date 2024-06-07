670826a12a2e5cb5618a08612fa42d04bad1f527
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2024-06-07 15:54:34 +0000

    adding a settings.json

--- src/alpine-common/install.sh
+++ src/alpine-common/install.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 set -e
 
+# Create the /root/.vscode-server/data/Machine/ directory if it does not exist
+mkdir -p /root/.vscode-server/data/Machine/
+
+# Create the /root/.vscode-server/data/Machine/settings.json if it does not exist
+touch /root/.vscode-server/data/Machine/settings.json
+
+# Set the "terminal.integrated.defaultProfile.linux" setting to "sh" in the settings.json
+echo '{"terminal.integrated.defaultProfile.linux": "sh"}' > settings.json
+
+# Copy the settings.json file to /root/.vscode-server/data/Machine/settings.json
+cp settings.json /root/.vscode-server/data/Machine/settings.json
