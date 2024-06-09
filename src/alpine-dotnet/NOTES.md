a4dab07329570706907dd8b2352a2ec4dea0b5d6
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2024-06-09 19:39:25 +0000

    installing dotnet

---
+++ src/alpine-dotnet/NOTES.md
@@ -0,0 +1,4 @@
+## links
+
+* <https://dotnet.microsoft.com/en-us/download/dotnet/scripts>
+* <https://learn.microsoft.com/en-gb/dotnet/core/tools/dotnet-install-script?WT.mc_id=dotnet-35129-website>
--- src/alpine-dotnet/install.sh
+++ src/alpine-dotnet/install.sh
@@ -1,1 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+curl -s -O https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+
+# TODO
+# 1. Add support for installing specific version of .NET Core SDK (options in devcontainer-feature.json)
+# 2. GPG: https://learn.microsoft.com/en-gb/dotnet/core/tools/dotnet-install-script?WT.mc_id=dotnet-35129-website#preparing-environment
+
+# `./dotnet-install.sh -Quality preview -Channel 9.0.x`
+
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
