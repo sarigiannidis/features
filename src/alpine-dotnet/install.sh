083f7654c2e34930ba82c0e353325855a15e58d5
Michalis Sarigiannidis <3284819+sarigiannidis@users.noreply.github.com>
2024-06-10 07:31:23 +0000

    reading the options in the script

--- src/alpine-dotnet/install.sh
+++ src/alpine-dotnet/install.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+# Read the options
+O_ARCHITECTURE="${ARCHITECTURE:-"<auto>"}"
+O_CHANNEL="${CHANNEL:-"<LTS>"}"
+O_QUALITY="${QUALITY:-"GA"}"
+O_RUNTIME="${RUNTIME:-"dotnet"}"
+O_VERSION=$VERSION
+
+# Download the install script
 curl -s -O https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+
+# Make the script executable
 chmod +x dotnet-install.sh
 
 # TODO
-# 1. Add support for installing specific version of .NET Core SDK (options in devcontainer-feature.json)
 # 2. GPG: https://learn.microsoft.com/en-gb/dotnet/core/tools/dotnet-install-script?WT.mc_id=dotnet-35129-website#preparing-environment
 
 # Install 
 
 `./dotnet-install.sh --channel 9.0.1xx --quality preview`
 
-echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
-echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
+# echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+# echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> ~/.bashrc
